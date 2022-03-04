d2f9793c6523ca45ac3309ff0a9109e345530623
sepehrgoodman1 <82294076+sepehrgoodman1@users.noreply.github.com>
2022-03-04 20:49:59 +0330

    first commit

---
+++ js/information.js
@@ -0,0 +1,9 @@
+// let input_link = document.querySelector('.input_link')
+
+// // if link address was too long, we will cut the link
+// input_link.addEventListener('input', ()=>{
+//     if(input_link.value.length>10){
+//         input_link.value = input_link.value.substring(0,20)
+//         console.log(temp_address)
+//     }
+// })
---
+++ js/table.js
@@ -0,0 +1,36 @@
+// select only header checkBox
+const selectAll = document.querySelector('.form-group.select_all input')
+
+// select all other checkboxes
+const allCheckBox = document.querySelectorAll('.form-group:not(.select_all) input')
+
+// detect if all check boxes selected or not
+let listBoolean = []
+allCheckBox.forEach(item=>{
+    item.addEventListener('change', function(){
+        allCheckBox.forEach(i=>{
+            listBoolean.push(i.checked);
+        })
+        if(listBoolean.includes(false)){
+            selectAll.checked = false;
+        }
+        else{
+            selectAll.checked = true;
+        }
+        listBoolean = []
+    })
+})
+
+// set checked all checkboxes 
+selectAll.addEventListener('change', function(){
+    if(this.checked){
+        allCheckBox.forEach(i=>{
+            i.checked = true;
+        })
+    }
+    else{
+        allCheckBox.forEach(i=>{
+            i.checked=false;
+        })
+    }
+})
