0e967b7bf866e2021408d034fd335539df200891
sepehrgoodman1 <82294076+sepehrgoodman1@users.noreply.github.com>
2022-03-06 16:22:49 +0330

    updated v1.2

--- js/information.js
+++ js/information.js
@@ -1,33 +1,39 @@
 // copy text to clipboard
-var artlink_clipboard = new ClipboardJS('#btn_artlink');
-var chain_clipboard = new ClipboardJS('#btn_chaincode');
+var artlink_clipboard = new ClipboardJS("#btn_artlink");
+var chain_clipboard = new ClipboardJS("#btn_chaincode");
 
-artlink_clipboard.on('success', function(e) {
-    console.info('Action:', e.action);
-    console.info('Text:', e.text);
-    console.info('Trigger:', e.trigger);
+artlink_clipboard.on("success", function (e) {
+  console.info("Action:", e.action);
+  console.info("Text:", e.text);
+  console.info("Trigger:", e.trigger);
 
-    e.clearSelection();
+  e.clearSelection();
 });
-artlink_clipboard.on('error', function(e) {
-    console.error('Action:', e.action);
-    console.error('Trigger:', e.trigger);
-});
-
-chain_clipboard.on('success', function(e) {
-    console.info('Action:', e.action);
-    console.info('Text:', e.text);
-    console.info('Trigger:', e.trigger);
-
-    e.clearSelection();
-});
-chain_clipboard.on('error', function(e) {
-    console.error('Action:', e.action);
-    console.error('Trigger:', e.trigger);
+artlink_clipboard.on("error", function (e) {
+  console.error("Action:", e.action);
+  console.error("Trigger:", e.trigger);
 });
 
-// add or remove class
-$( '.btn_timeLine' ).click(function() {
-    $( '.btn_timeLine' ).removeClass('btn_active_timeline')
-    $( this ).addClass('btn_active_timeline')
-  });
+chain_clipboard.on("success", function (e) {
+  console.info("Action:", e.action);
+  console.info("Text:", e.text);
+  console.info("Trigger:", e.trigger);
+
+  e.clearSelection();
+});
+chain_clipboard.on("error", function (e) {
+  console.error("Action:", e.action);
+  console.error("Trigger:", e.trigger);
+});
+
+// add or remove class ( time line buttons )
+$(".btn_timeLine").click(function () {
+  $(".btn_timeLine").removeClass("btn_active_timeline");
+  $(this).addClass("btn_active_timeline");
+});
+
+// switch buttons in proofs page
+$(".btn_section").click(function () {
+  $(".btn_section").removeClass("btn_section_active");
+  $(this).addClass("btn_section_active");
+});
